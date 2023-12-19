e74b91e5f0fbd584f87b4a1a235d556be1cc7d64
Caroline de Souza Carvalho <124586812+CarolineSCarvalho@users.noreply.github.com>
2023-12-19 19:57:56 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,43 @@
+function tocaSom (seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);  
+
+  if (elemento === null){
+    alert('Elemento não encontrado');
+  }
+  
+  if (elemento != null){
+    elemento.play();
+  }
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+  const tecla = listaDeTeclas[contador];
+    const instrumento = listaDeTeclas[contador].classList[1];
+    
+
+    const idAudio = `#som_${instrumento}`;
+    
+    //console.log(idAudio);
+
+    tecla.onclick = function () {
+      tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+      if (evento.code === 'Space' || evento.code === 'Enter') {
+         tecla.classList.add('ativa');
+      }
+    }
+
+    tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
+    }
+
+
+
+  }
